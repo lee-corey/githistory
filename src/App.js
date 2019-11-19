f985a0150ca5191cd25f7f67317b1d988f96dc91
Warrior <warrior2110@outlook.com>
2019-11-19 10:49:34 -0800

    githublogin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GitHubLogin from 'react-github-login';
+
+const onSuccess = response => console.log(response);
+const onFailure = response => console.error(response);
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <GitHubLogin clientId="ac56fad434a3a3c1561e"
+          onSuccess={onSuccess}
+          onFailure={onFailure}/>
       </header>
     </div>
   );
