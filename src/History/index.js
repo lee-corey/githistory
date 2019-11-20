934e2b07afe62765d3bf62da4288552ebe47dd63
Warrior <warrior2110@outlook.com>
2019-11-20 04:08:01 -0800

    make container

---
+++ src/History/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from 'react-bootstrap'
+
+function History() {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default History;
--- src/MainApp/index.js
+++ src/MainApp/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './style.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import History from '../History';
 
 function MainApp() {
     return (
-        <div>
-            Hello
-        </div>
+        <Container>
+            <Row>
+                <Col className="header">
+                    <Row className="headerText">
+                        Git commit history
+                    </Row>
+                </Col>
+                <Col className="main">
+                    <History />
+                </Col>
+                <Col className="footer">
+                    <Row className="footerText">
+                        Developed by MebDevelop
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export default MainApp;
