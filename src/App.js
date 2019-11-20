71f781fd960875c3b25153d98f852c1ea573e108
Warrior <warrior2110@outlook.com>
2019-11-19 18:51:00 -0800

    add and remove packages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import GitHubLogin from 'react-github-login';
-
-const onSuccess = response => console.log(response);
-const onFailure = response => console.error(response);
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <GitHubLogin clientId="ac56fad434a3a3c1561e"
-          onSuccess={onSuccess}
-          onFailure={onFailure}/>
+        
       </header>
     </div>
   );
