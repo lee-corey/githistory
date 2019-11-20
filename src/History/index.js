62e80ea15a715dab6bd12d7fe81a2226c598c2a0
Warrior <warrior2110@outlook.com>
2019-11-20 05:15:43 -0800

    show history

--- src/History/index.js
+++ src/History/index.js
@@ -1,52 +1,72 @@
 import React from 'react';
-import {Container, ListGroup    } from 'react-bootstrap'
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+import {Container, ListGroup} from 'react-bootstrap'
+import Loading from '../Loading';
+import ErrorMessage from '../Error';
+
+const GET_COMMIT_HISTORY = gql`
+{
+    repository(name: "githistory", owner: "MebDevelop") {
+      ref(qualifiedName: "master") {
+        target {
+          ... on Commit {
+            id
+            history(first:100) {
+              pageInfo {
+                hasNextPage
+              }
+              edges {
+                node {
+                  messageHeadline
+                  oid
+                  message
+                  author {
+                    name
+                    email
+                    date
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+}  
+`;
 
 function History() {
     return (
-        <Container>
-            <ListGroup variant="flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            </ListGroup>
-        </Container>
+        <Query
+            query={GET_COMMIT_HISTORY}
+            notifyOnNetworkStatusChange={true}
+        >
+            {({ data, loading, error, fetchMore }) => {
+                console.log(data)
+                if (loading && !data) {
+                    return <Loading isCenter={true} />;
+                }
+            
+                if (error) {
+                    return <ErrorMessage error={error} />;
+                }
+                const edges = data.repository.ref.target.history.edges;
+                return (
+                    <Container>
+                        <ListGroup variant="flush">
+                            {
+                                edges.map((each, i) => { 
+                                    return (
+                                        <ListGroup.Item key = {i}>{each.node.message}</ListGroup.Item>
+                                    )
+                                })
+                            }
+                        </ListGroup>
+                    </Container>
+                )
+            }}
+        </Query>
     )
 }
 
